51b65aa0d961defaa6f832caa3098475ef9abcda
Christine Maynard <christinekrm02@gmail.com>
2021-01-26 17:58:32 -0500

    To create a Class for each resource See code

--- public/app.js
+++ public/app.js
@@ -1,12 +1,27 @@
 "use strict";
+var Resource = /** @class */ (function () {
+    //Initialize values in constructor to pass values for each properties to instance of class
+    function Resource(title, details, rating) {
+        this.title = title;
+        this.details = details;
+        this.rating = rating;
+    }
+    Resource.prototype.format = function () {
+        return "The article " + this.title + " explains the " + this.details + ". I give it a rating of " + this.rating;
+    };
+    return Resource;
+}());
+var resourceOne = new Resource("More on React Redux", "how to connect to redux store and more", 9);
+var resourceTwo = new Resource("CSS Tips and Tricks", "fun animations with CSS", 7);
+console.log(resourceOne, resourceTwo);
 //to select input fields
 var type = document.querySelector("#type");
 var topic = document.querySelector("#topic-title");
-var details = document.querySelector("#details");
+var detailss = document.querySelector("#detailss");
 var dateExplored = document.querySelector("#date-explored");
 //to select form
 var form = document.querySelector(".new-item-form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log(type.value, topic.value, details.value, dateExplored.value);
+    console.log(type.value, topic.value, detailss.value, dateExplored.value);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,36 @@
+class Resource {
+  title: string;
+  details: string;
+  rating: number;
+  //Initialize values in constructor to pass values for each properties to instance of class
+  constructor(title: string, details: string, rating: number) {
+    this.title = title;
+    this.details = details;
+    this.rating = rating;
+  }
+  format() {
+    return `The article ${this.title} explains the ${this.details}. I give it a rating of ${this.rating}`;
+  }
+}
+
+const resourceOne = new Resource(
+  "More on React Redux",
+  "how to connect to redux store and more",
+  9
+);
+const resourceTwo = new Resource(
+  "CSS Tips and Tricks",
+  "fun animations with CSS",
+  7
+);
+console.log(resourceOne, resourceTwo);
+let resources = Resource[]=[];
+
+//Add to DOM
 //to select input fields
 const type = document.querySelector("#type") as HTMLSelectElement;
 const topic = document.querySelector("#topic-title") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
+const detailss = document.querySelector("#detailss") as HTMLInputElement;
 const dateExplored = document.querySelector(
   "#date-explored"
 ) as HTMLInputElement;
@@ -10,5 +39,5 @@ const form = document.querySelector(".new-item-form") as HTMLFontElement;
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  console.log(type.value, topic.value, details.value, dateExplored.value);
+  console.log(type.value, topic.value, detailss.value, dateExplored.value);
 });
